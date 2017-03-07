6360d5fa55ea1135e5e4d44186c7be3e93e70b97
Micheál Looney <hello@micheallooney.com>
2017-03-07 02:00:15 +0000

    Create user in Intercom

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'http://rubygems.org'
 
 gem 'frinkiac', '~> 0.0.4'
-
+gem 'intercom', '~> 3.5.10'
 
--- main.rb
+++ main.rb
@@ -1,5 +1,9 @@
 require 'json'
 require 'frinkiac'
+require 'intercom'
+
+token = ENV["INTERCOM_PAT"]
+intercom = Intercom::Client.new(token: token)
 
 file = File.read "./output.json"
 data = JSON.parse(file)
@@ -9,16 +13,17 @@ def get_image(name)
   if(scr != nil) 
     scr.image_url
   end
-
 end
 
 data.each do |d|
     name = d["name"]
+    email = d["email"]
     puts name
     avatar_url = get_image(name)
-    puts avatar_url
+    user = intercom.users.create(email: email, name: name)
+    user.avatar.image_url = avatar_url
+    intercom.users.save(user)
 end
 
-#data.each { |d| puts d["name"] }
 
 
